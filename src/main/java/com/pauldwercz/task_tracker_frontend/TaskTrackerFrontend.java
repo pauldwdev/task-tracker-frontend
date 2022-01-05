7067861b07ed9b48a7e70d8396801cd4a871a7cb
paul <paul@PaulDW-LPTP.fios-router.home>
2022-01-04 21:22:18 -0500

    Initial commit.

---
+++ src/main/java/com/pauldwercz/task_tracker_frontend/TaskTrackerFrontend.java
@@ -0,0 +1,11 @@
+package com.pauldwercz.task_tracker_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerFrontend {
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerFrontend.class, args);
+	}
+}
